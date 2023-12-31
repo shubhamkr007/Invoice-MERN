59628ade55cf1bc9897ff16e01cceaee02380c87
SHUBHAM KUMAR <shubhamkumarsinghbba@gmail.com>
2024-01-01 00:50:08 +0530

    feat: add logging with winston and morgan

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ import cookieParser from 'cookie-parser'
 import "dotenv/config"
 import express from 'express'
 import morgan from 'morgan'
+import {morganMiddleware,systemLogs} from './utils/Logger.js'
 
 const app= express();
 
@@ -14,6 +15,8 @@ app.use(express.json());
 
 app.use(express.urlencoded({extended:false}))
 app.use(cookieParser());
+
+app.use(morganMiddleware);
 app.get("/api/v1/test",(req,res)=>{
     res.json({Hi:"Welcome to the Invoice App"})
 })
@@ -24,5 +27,9 @@ app.listen(PORT, ()=>{
     console.log(
         `${chalk.green.bold("👍")} Server Running in ${chalk.yellow.bold(process.env.NODE_ENV)} mode on 
         port :-- ${chalk.cyan(PORT)}`
-        )
+    )
+
+    systemLogs.info(
+        `Server running in ${process.env.NODE_ENV} mode on port ::: ${PORT}`
+    )
 })
