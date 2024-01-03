e2cc9f4fe53f07610bd5e1f765c6afecc9c80130
SHUBHAM KUMAR <shubhamkumarsinghbba@gmail.com>
2024-01-03 16:30:37 +0530

    feat: add senEmail, and email templates

---
+++ backend/utils/sendEmail.js
@@ -0,0 +1,32 @@
+import "dotenv/config"
+import fs from "fs"
+import path from "path"
+import handlebars from "handlebars"
+import { fileURLToPath } from "url"
+import transporter from '../helpers/emailTransport.js'
+import { systemLogs } from "./Logger.js"
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const sendEmail = async (email, subject, payload, template) => {
+    try {
+        const sourceDirectory = fs.readFileSync(
+            path.join(__dirname, template),
+            "utf8",
+        )
+
+        const compiledTemplate = handlebars.compile(sourceDirectory)
+        const emailOptions = {
+            from: process.env.SENDER_EMAIL,
+            to: email,
+            subject: subject,
+            html: compiledTemplate(payload),
+        }
+
+        await transporter.sendEmail(emailOptions)
+    } catch (error) {
+        systemLogs.error(`Email not Sent: ${error}`)
+    }
+}
+
+export default sendEmail;
